f7f354a27c48b8f6130b0ef7e3c7dee24b6c7d6b
din <din090bmt@gmai.com>
2022-12-24 21:33:53 +0700

    add unit test login

---
+++ UnitTestLogin/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestLogin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestLogin")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c607147e-95fe-4d64-8ec4-5e07a3b9e2b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestLogin/UnitTest1.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QLDL;
+using System;
+
+namespace UnitTestLogin
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private frmLogin f;
+        [TestInitialize]
+        public void setUp()
+        {
+            f = new frmLogin();
+        }
+
+        [TestMethod]
+        public void TestLoginAccount1()
+        {
+            bool expected, actual;
+            expected = true;
+            actual = f.checkAccount("tranhoangquangdin", "0123456");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestLoginAccount2()
+        {
+            bool expected, actual;
+            expected = true;
+            actual = f.checkAccount("tranhoangquangdin", "0123456");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestLoginAccount3()
+        {
+            bool expected, actual;
+            expected = true;
+            actual = f.checkAccount("hoanglydieu", "abcdef");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestLoginAccount4()
+        {
+            bool expected, actual;
+            expected = true;
+            actual = f.checkAccount("doanphuongnam", "987654");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestLoginAccount5()
+        {
+            bool expected, actual;
+            expected = true;
+            actual = f.checkAccount("truongthingantram", "123321");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestLoginAccount6()
+        {
+            bool expected, actual;
+            expected = true;
+            actual = f.checkAccount("admin", "123456");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
